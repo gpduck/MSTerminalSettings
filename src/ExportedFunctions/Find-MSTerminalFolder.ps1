de20cde0e6ceba09d67aad7984304743f265b5d2
Chris Duck <gpduck@whatsupduck.net>
2019-06-23 02:49:59 -0500

    Fix missing comma

--- src/ExportedFunctions/Find-MSTerminalFolder.ps1
+++ src/ExportedFunctions/Find-MSTerminalFolder.ps1
@@ -2,7 +2,7 @@ function Find-MSTerminalFolder {
     $Paths = @(
         (Join-Path $env:LOCALAPPDATA "packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/RoamingState"),
         (Join-Path $env:APPDATA "Microsoft/Windows Terminal"),
-        (Join-Path $env:LOCALAPPDATA "packages/WindowsTerminalDev_8wekyb3d8bbwe/RoamingState"),
+        (Join-Path $env:LOCALAPPDATA "packages/WindowsTerminalDev_8wekyb3d8bbwe/RoamingState")
     )
     foreach($Path in $Paths) {
         if(Test-Path $Path) {
