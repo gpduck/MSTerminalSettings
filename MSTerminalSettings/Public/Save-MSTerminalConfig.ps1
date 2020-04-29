331fca0055483c689c3e662e9e74cfb9b7e7a493
Justin Grote <jgrote@allieddigital.net>
2020-04-28 21:57:32 -0700

    Fix WinPS5.1 writing in wrong encoding

--- MSTerminalSettings/Public/Save-MSTerminalConfig.ps1
+++ MSTerminalSettings/Public/Save-MSTerminalConfig.ps1
@@ -10,7 +10,15 @@ function Save-MSTerminalConfig {
     if ($TerminalConfig -and -not $Path -and -not $TerminalConfig.Path) {throw 'You specified a generated MSTerminalConfig object, and therefore must specify an output path with -Path to use this command'}
     if (-not $Path -and $TerminalConfig.Path) {$Path = $TerminalConfig.Path}
     if ($PSCmdlet.ShouldProcess($TerminalConfig.Path,'Saving Terminal Settings to File')) {
-        [Serialize]::ToJson($TerminalConfig) > $Path
+
+        $configToSave = [Serialize]::ToJson($TerminalConfig)
+        #Powershell 5.1 Compatible Option to output UTF8 without BoM
+        #TODO: Remove when Powershell 5.1 support is dropped
+        if ($PSEdition -eq 'Desktop') {
+            [io.file]::WriteAllLines($Path,$configToSave,[text.utf8encoding]::new($false))
+        } else {
+            Out-File -InputObject $configToSave -Encoding utf8NoBOM -FilePath $Path -Force
+        }
         #Slight pause to allow Windows Terminal to catch up
         #TODO: Some sort of event maybe?
         sleep 0.1
